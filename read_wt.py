8da877c5fe249a8715d5738e3d5e4b2272077a8e
me <my@email.com>
2020-02-06 16:35:30 +0300

    - переделано из цикла на работу со списками TODO сделать поиск и устранение скачков плотности 	modified:   read_wt.py

--- read_wt.py
+++ read_wt.py
@@ -1,4 +1,4 @@
-import sys
+import sys, re
 from datetime import datetime, timedelta
 
 DOIL=0.814
@@ -6,13 +6,9 @@ DWAT=1.059
 REF =2434
 NULL = 0.01
 
-
-
 def get_gauge_tvdss_from_db_output(well_name, approximate_date): 
-    #f_date = '14.05.2019'
-    #f_well = 'WQ2-178'
     with open('wt_gauges_MD_TVDSS', 'r') as gauge_db: # DBout filename hardcoded !!
-        lines = [ [abs(datetime.strptime(y.split()[1],"%d.%m.%Y")-datetime.strptime(approximate_date,"%d.%m.%Y")), y.split()[0], y.split()[3], y.split()[4]] for y in [x.strip() for x in gauge_db] if y.split()[0]==well_name] 
+        lines = [ [abs(datetime.strptime(y.split()[1],"%d.%m.%Y")-approximate_date), y.split()[0], y.split()[3], y.split()[4]] for y in [x.strip() for x in gauge_db] if y.split()[0]==well_name] 
         filter_line = [x for x in lines if x[0] == min(map(lambda x: x[0], lines))]
         #print(filter_line) # debug
         return float(filter_line[0][3]) # 
@@ -28,56 +24,41 @@ def derivative(x1, x2, t1, t2):
 
 
 def read_WT_hist_file(file_name):
-    oil, water, wcut, gauge_tvdss, press_mix = 0, 0, 0, 0, 0
-    prev_press, prev_date_time = 0, datetime(year=1900, month=1, day=1)
-    dmix = DOIL
-    prev_dmix = DOIL
+    well = ''
+    dates, qoil, qwat, bhp, thp, wcut = [], [], [], [], [], []
 
     with open(file_name,'r') as input_file:
-        lines = [x.strip() for x in input_file]
-        #filter_lines = [x for x in lines[lines.index('HTAB')+1:lines.index('ENDH')] if x[0:2]!='--']
+        all_content = ''.join([x for x in input_file])
+        #all_before_htab = re.search(r"[\w\W]*HTAB", all_content) 
+        #all_after_htab = re.search("ENDH[\w\W]*", all_content)
         
-        #well_type = lambda orat, wrat : 'Water injector' if orat+wrat==wrat else 'Oil producer' if orat>0 else 'Undefined' 
+        htab_lines = re.findall(r"(.*)\n",  re.search(r"HTAB.*\n([\W\w]*)ENDH", all_content).group(1))
+        htab_records = [x for x in htab_lines if x[0:2]!="--"]
+        
+        for x in htab_records:
+            words = re.split(r'\s+|/', x)
+            well = words[0]
+            dates.append(datetime.strptime(words[1], "%d.%m.%Y")+timedelta(hours=float(words[8])))
+            qoil.append(float(words[2]))
+            qwat.append(float(words[3]))
+            bhp.append(float(words[4]))
+            thp.append(words[5])
 
-        is_htab=False
+    g_tvdss = get_gauge_tvdss_from_db_output(well, dates[0]) 
+    wcut = [w/(o+w) for o,w in zip(qoil, qwat)]
+    mix_den = [(1-w)*DOIL+w*DWAT for w in wcut]
+    bhp_mix = [p+(REF-g_tvdss)*9.81*(dm-DOIL)/100 if p > 0.01 else p for p,dm in zip(bhp, mix_den)]
 
-        for x in lines: 
-            if x == 'ENDH': 
-                is_htab=False
+    for d,dt,x,y,w in zip(mix_den, dates, bhp, bhp_mix, wcut):
+        #print(f"{well} {dt:%d.%m.%Y} {g_tvdss:.2f} {d:.3f} {w:.2f} {x:.2f} {y:.2f}")
+        print(f"{well} {dt:%d.%m.%Y} {g_tvdss:.2f} {d:.3f} {w:.2f} {x:.2f} {y:.2f}") # htab format
 
-
-            if is_htab and x[0:2]!='--': 
-                words = x.split()
-                gauge_tvdss = get_gauge_tvdss_from_db_output(words[0], words[1])
-
-                oil, water, press_oil = float(words[2]), float(words[3]), float(words[4])
-                date_time = datetime.strptime(words[1], "%d.%m.%Y") + timedelta(hours=float(words[8]))
-                wcut =  water/(oil+water)
-
-                dP = derivative(prev_press, press_oil, prev_date_time, date_time)
-                dmix = (wcut*DWAT+(1-wcut)*DOIL)
-
-                if press_oil > NULL:
-                    press_mix = press_oil+(REF-gauge_tvdss)*9.81*(dmix-DOIL)/100
-                else: 
-                    press_mix = press_oil
-
-
-
-                #print(f"{wcut:.2f} {gauge_tvdss:.1f} {press_mix:.3f} {x}") # debug
-                #print(f"{derivative(prev_press, press_oil, prev_date_time, date_time)} {wcut} {x}") # debug
-                print(*words[0:4], press_mix, *words[5:])
-
-                prev_press  = press_oil
-                prev_date_time = date_time
-                prev_dmix = dmix
-            else:
-                print(x) # print other lines
-                #pass
-            
-
-            if x == 'HTAB': 
-                is_htab=True
+    #print(well)
+    #print(dates)
+    #print(qoil)
+    #print(qwat)
+    #print(bhp)
+    #print(thp)
 
 
 
@@ -86,5 +67,4 @@ read_WT_hist_file(sys.argv[1])
 # TODO get TVDSS for current WT (BasPro) 
 #     partly DONE(needs check)
 #     unloads odd values with wrong MD-TVDSS for PLT-tests (need test filter in oracle-query)
-#
         
