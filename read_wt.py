fe2cec8b336e5d0f6e9d0f8d526a90a05571fad9
me <my@email.com>
2020-02-07 15:59:24 +0300

    - добавлен массив для сглаживания скачков плотности 	modified:   read_wt.py

--- read_wt.py
+++ read_wt.py
@@ -25,7 +25,7 @@ def derivative(x1, x2, t1, t2):
 
 def read_WT_hist_file(file_name):
     well = ''
-    dates, qoil, qwat, bhp, thp, wcut = [], [], [], [], [], []
+    dates, qoil, qwat, bhp, thp, wcut, hours = [], [], [], [], [], [], []
 
     with open(file_name,'r') as input_file:
         all_content = ''.join([x for x in input_file])
@@ -43,15 +43,50 @@ def read_WT_hist_file(file_name):
             qwat.append(float(words[3]))
             bhp.append(float(words[4]))
             thp.append(words[5])
+            hours.append(words[8])
 
     g_tvdss = get_gauge_tvdss_from_db_output(well, dates[0]) 
     wcut = [w/(o+w) for o,w in zip(qoil, qwat)]
     mix_den = [(1-w)*DOIL+w*DWAT for w in wcut]
     bhp_mix = [p+(REF-g_tvdss)*9.81*(dm-DOIL)/100 if p > 0.01 else p for p,dm in zip(bhp, mix_den)]
 
-    for d,dt,x,y,w in zip(mix_den, dates, bhp, bhp_mix, wcut):
-        #print(f"{well} {dt:%d.%m.%Y} {g_tvdss:.2f} {d:.3f} {w:.2f} {x:.2f} {y:.2f}")
-        print(f"{well} {dt:%d.%m.%Y} {g_tvdss:.2f} {d:.3f} {w:.2f} {x:.2f} {y:.2f}") # htab format
+
+    regime = [1 if x+y > NULL else 0 for x,y in zip(qoil, qwat) ]
+    regime_shift = regime[:] 
+    regime_shift.insert(-1,regime_shift[-1])
+    regime_shift.pop(0)
+    ch_moments = [0 if x==y else 1 for x,y in zip(regime, regime_shift)]
+    ch_indexes = [i for i,x in enumerate(ch_moments) if x==1]  
+
+    #print(regime)
+    #print(regime_shift)
+    #print(ch_moments)
+    #print(ch_indexes)
+
+
+    cc = 0
+    ccc = []
+    for i in enumerate(ch_moments):
+        if i[0] in ch_indexes:
+            cc = i[0]
+        ccc.append(cc)
+    #print(ccc)
+
+    date_indexes = [x[0] if x[1]==0 else x[1] for x in enumerate(ccc)]
+    #print(date_indexes)
+    deltas = [x-dates[i] for x,i in zip(dates,date_indexes)]
+
+    #for i in range(len(dates)):
+    #    print(dates[i], dates[date_indexes[i]], deltas[i].total_seconds()/60/60)
+
+    dt_deltas = [x.total_seconds()/60/60 if x.total_seconds()/60/60 < 1 else 0 for x in deltas]
+    #print(dt_deltas)
+
+
+
+    for mix_den_i, dates_i, bhp_i,bhp_mix_i, wcut_i, qoil_i, qwat_i, thp_i, hours_i, dt_deltas_i in zip(mix_den, dates, bhp, bhp_mix, wcut, qoil, qwat, thp, hours, dt_deltas):
+        print(f"{well} {dates_i:%d.%m.%Y %H:%M:%S} {g_tvdss:.2f} {wcut_i:.3f} {bhp_i:.2f} {bhp_mix_i:.2f} {mix_den_i:.3f} {dt_deltas_i:.5f}") # debug format
+        #print(f"{well} {dates_i:%d.%m.%Y} {qoil_i:10.3f} {qwat_i:10.3f} {bhp_mix_i:10.2f} {thp_i} 1 Hours {hours_i}") # htab format
 
     #print(well)
     #print(dates)
@@ -64,6 +99,8 @@ def read_WT_hist_file(file_name):
 
 read_WT_hist_file(sys.argv[1])
 
+# TODO smooth density change when well goes to PBU/from PBU
+# TODO find out how density changes PI calculation!!
 # TODO get TVDSS for current WT (BasPro) 
 #     partly DONE(needs check)
 #     unloads odd values with wrong MD-TVDSS for PLT-tests (need test filter in oracle-query)
