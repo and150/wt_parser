a1f04b138a0a8c44160be4255bab924c92eff24e
me <my@email.com>
2020-02-14 15:15:29 +0300

    mixture density smoothed - the first variant is based on constant transition length (1 hour)
    PROBLEM: it is better to calculate
    derivative and choose only periods of fast pressure changes
    	modified:   read_wt.py

--- read_wt.py
+++ read_wt.py
@@ -6,7 +6,7 @@ DWAT=1.059
 REF =2434
 NULL = 0.01
 WCUT_TRIGGER = 0.05 # check if i have written somewhere 0.025 (PO or anywhere else)
-DENS_CHANGE = 1 # hours for mixture separation or vice versa
+DC = 0.75 # hours for mixture separation or vice versa
 
 def get_gauge_tvdss_from_db_output(well_name, approximate_date): 
     with open('wt_gauges_MD_TVDSS', 'r') as gauge_db: # DBout filename hardcoded !!
@@ -57,10 +57,6 @@ def read_WT_hist_file(file_name):
     elif max(wcut) < WCUT_TRIGGER:
         return (all_content, f"Wcut is less {WCUT_TRIGGER}, no recalculation ")
     else:
-        gauge_md, gauge_tvdss  = get_gauge_tvdss_from_db_output(well, dates[0]) 
-        mix_den = [(1-w)*DOIL+w*DWAT for w in wcut]
-        bhp_mix = [p+(REF-gauge_tvdss)*9.81*(dm-DOIL)/100 if p > 0.01 else p for p,dm in zip(bhp, mix_den)]
-        
         regime = [1 if x+y > NULL else 0 for x,y in zip(qoil, qwat) ]
         ch_moments = [0 if x==y else 1 for x,y in zip(regime, regime[1:]+[regime[-1]])]
         ch_indexes = [i for i,x in enumerate(ch_moments) if x==1]  
@@ -70,19 +66,22 @@ def read_WT_hist_file(file_name):
             if i[0] in ch_indexes:
                 cc = i[0]
             ccc.append(cc)
-        #print(ccc)
+        #print(ccc) # additional array of change regime indexes
         date_indexes = [x[0] if x[1]==0 else x[1] for x in enumerate(ccc)]
         deltas = [x-dates[i] for x,i in zip(dates,date_indexes)]
         #for i in range(len(dates)):
         #    print(dates[i], dates[date_indexes[i]], deltas[i].total_seconds()/60/60)
-        dt_deltas = [x.total_seconds()/60/60 if x.total_seconds()/60/60 < DENS_CHANGE else 0 for x in deltas] # print hours from regime change within one hour
+        dt_deltas = [x.total_seconds()/60/60 if x.total_seconds()/60/60 < DC else 0 for x in deltas] # print hours from regime change within one hour
         #print(dt_deltas)
-        
-        for mix_den_i, dates_i, bhp_i,bhp_mix_i, wcut_i, qoil_i, qwat_i, thp_i, hours_i, dt_deltas_i in zip(mix_den, dates, bhp, bhp_mix, wcut, qoil, qwat, thp, hours, dt_deltas):
-            #print(f"{well} {dates_i:%d.%m.%Y %H:%M:%S} {gauge_tvdss:.2f} {wcut_i:.3f} {bhp_i:.2f} {bhp_mix_i:.2f} {mix_den_i:.3f} {dt_deltas_i:.5f}") # debug format
-            #print(f"{well} {dates_i:%d.%m.%Y} {qoil_i:10.3f} {qwat_i:10.3f} {bhp_mix_i:10.2f} {thp_i} 1 Hours {hours_i}") # htab format
+
+        gauge_md, gauge_tvdss  = get_gauge_tvdss_from_db_output(well, dates[0]) 
+        mix_den = [(1-w)*DOIL+w*DWAT for w in wcut]
+        mix_den_smooth = [x if dt==0 else x*dt/DC+mix_den[c]*(DC-dt)/DC for x,c,dt in zip(mix_den,ccc,dt_deltas)]
+        bhp_mix = [p+(REF-gauge_tvdss)*9.81*(dm-DOIL)/100 if p > 0.01 else p for p,dm in zip(bhp, mix_den_smooth)]
+
+        for mix_den_i, mix_den_smooth_i, dates_i, bhp_i,bhp_mix_i, wcut_i, qoil_i, qwat_i, thp_i, hours_i, dt_deltas_i in zip(mix_den, mix_den_smooth, dates, bhp, bhp_mix, wcut, qoil, qwat, thp, hours, dt_deltas):
+            #recalculated_htab += f"{well} {dates_i:%d.%m.%Y %H:%M:%S} {gauge_tvdss:.2f} {wcut_i:.3f} {bhp_i:.2f} {bhp_mix_i:.2f} {mix_den_i:.3f} {mix_den_smooth_i:.3f} {dt_deltas_i:.5f}\n" # debug format
             recalculated_htab += f"{well} {dates_i:%d.%m.%Y} {qoil_i:10.3f} {qwat_i:10.3f} {bhp_mix_i:10.2f} {thp_i} 1 Hours {hours_i}\n" # output htab
-            #pass
         return (all_before_htab + recalculated_htab + all_after_htab, f"RECALCULATED,\tmax_wcut={max(wcut):.3f}\tgauge_md={gauge_md:.2f}\tgauge_tvdss={gauge_tvdss:.2f}")
 
 
@@ -114,7 +113,7 @@ process_all_files()
 #output, out_log = read_WT_hist_file(sys.argv[1])
 #print(output)
 
-# TODO smooth density change when well goes to PBU/from PBU
+# TODO smooth density change when well goes to PBU/from PBU  -  first variant done
 # TODO find out how density influences the results of PI calculation!!
 # TODO get TVDSS for current WT (BasPro) > partly DONE(needs check) > gives odd values with wrong MD-TVDSS for PLT-tests (need test filter in oracle-query)
         
